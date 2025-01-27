5412db36957e8d619b041ccffc95c30cfde3446d
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-27 13:02:30 -0400

    refactor: renamed constants

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Check in which mode is being executed
-export const args =process.argv.slice(2)
-export const isDev = args.includes('--dev')
-export const isProd = args.includes('--prod')
-export const isDebug = args.includes('--debug')
-export const save = args.includes('--save')
+export const ARGS =process.argv.slice(2)
+export const IS_DEV = ARGS.includes('--dev')
+export const IS_PROD = ARGS.includes('--prod')
+export const IS_DEBUG = ARGS.includes('--debug')
+export const SAVE = ARGS.includes('--save')
 
 // Mode keys
-export const dev= "dev"
-export const prod ="prod"
-export const debug ="debug"
+export const DEV= "dev"
+export const PROD ="prod"
+export const DEBUG ="debug"
 export let mode
 
 // Check if there are multiple modes set
-if ([isDev, isProd, isDebug].filter(cond=>cond).length>1)
+if ([IS_DEV, IS_PROD, IS_DEBUG].filter(cond=>cond).length>1)
     throw new Error('Multiple modes set')
 
 // Set the mode according to the running environment
-if (isDev)
-    mode=dev
-else if (isProd)
-    mode=prod
-else if (isDebug)
-    mode=debug
+if (IS_DEV)
+    mode=DEV
+else if (IS_PROD)
+    mode=PROD
+else if (IS_DEBUG)
+    mode=DEBUG
