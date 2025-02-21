38064333f41c5f445e78333e98e71d6d98daf151
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-02-20 20:43:20 -0400

    feat: added setupVite environment function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Check in which mode is being executed
-let ARGS, IS_DEV, IS_PROD, IS_DEBUG, SAVE, MODE, DEV, PROD, DEBUG, MIGRATE;
+let ARGS, IS_DEV, IS_PROD, IS_DEBUG, SAVE, MODE, MIGRATE;
+let DEV = 'dev', PROD = 'prod', DEBUG = 'debug';
 export { IS_DEV, IS_PROD, IS_DEBUG, SAVE , MIGRATE, MODE, DEV, PROD, DEBUG };
 
 // Check which mode is being executed
 function checkMode() {
-    if ([IS_DEV, IS_PROD, IS_DEBUG].filter(cond=>cond).length>1)
+    const activeModes = [IS_DEV, IS_PROD, IS_DEBUG].filter(mode=>mode)
+    if (activeModes.length>1)
         throw new Error('Multiple modes set')
+    if (activeModes.length===0)
+        throw new Error('No mode set')
+
     if (IS_DEV)
         MODE=DEV
     else if (IS_PROD)
@@ -22,13 +27,23 @@ export function loadNode() {
     IS_DEBUG = ARGS.includes('--debug')
     SAVE = ARGS.includes('--save')
     MIGRATE = ARGS.includes('--migrate')
+    checkMode()
 }
 
-// Load Vite environment.
+// Load Vite environment
 export function loadVite() {
-    IS_DEV = import.meta.env.MODE==='dev'
-    IS_PROD = import.meta.env.MODE==='prod'
-    IS_DEBUG = import.meta.env.MODE==='debug'
-    SAVE = import.meta.env.SAVE==='true'
-    MIGRATE = import.meta.env.MIGRATE==='true'
+    ARGS = process.argv.slice(2)
+    const modeIndex = ARGS.indexOf('--mode')
+    if (modeIndex>=0&& ARGS.length>modeIndex+1){
+        IS_DEV = ARGS[modeIndex+1]===DEV
+        IS_PROD = ARGS[modeIndex+1]===PROD
+        IS_DEBUG = ARGS[modeIndex+1]===DEBUG
+    }else{
+        IS_DEV = false
+        IS_PROD = false
+        IS_DEBUG = false
+    }
+    SAVE = ARGS.includes('--save')
+    MIGRATE = ARGS.includes('--migrate')
+    checkMode()
 }
